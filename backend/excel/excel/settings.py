1af25501c4f7153c480fdf24cdb5cfea92bf1adf
anishad6 <73513343+anishad6@users.noreply.github.com>
2025-07-14 15:11:01 +0530

    Update settings.py

--- backend/excel/excel/settings.py
+++ backend/excel/excel/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'excel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'attandace',        
+        'USER': 'postgres',       
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        # 'PORT': '5432',                      
     }
 }
+
 
 
 # Password validation
