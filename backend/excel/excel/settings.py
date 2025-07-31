03a84b994b8430759771d6d831c9af5bc2a1ce69
anishad6 <73513343+anishad6@users.noreply.github.com>
2025-07-31 14:51:00 +0530

    Update settings.py

--- backend/excel/excel/settings.py
+++ backend/excel/excel/settings.py
@@ -172,3 +172,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if os.environ.get('RENDER'):
+    MEDIA_ROOT = '/tmp'
