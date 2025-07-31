e372ef3164c21bb824094780bc79f260376dd191
anishad6 <73513343+anishad6@users.noreply.github.com>
2025-07-31 12:30:45 +0530

    Update urls.py

--- backend/excel/app/urls.py
+++ backend/excel/app/urls.py
@@ -1,11 +1,25 @@
+
+# new one
 from django.urls import path
 from app.views import  upload_excel,generate_attendance_summary,upload_salary,save_rules
 urlpatterns = [
     path('upload-excel/', upload_excel, name='upload_excel'),
     path('generate-summary/', generate_attendance_summary, name='generate_attendance_summary'),
     path('upload-salary/', upload_salary, name='upload_salary'),
-    path('save-rules/', save_rules)  
+    path('save-rules/', save_rules),
+    # path('calculate-salary/', calculate_salary, name='calculate_salary'),
+ 
 ]
+
+
+# from django.urls import path
+# from app.views import  upload_excel,generate_attendance_summary,upload_salary,save_rules
+# urlpatterns = [
+#     path('upload-excel/', upload_excel, name='upload_excel'),
+#     path('generate-summary/', generate_attendance_summary, name='generate_attendance_summary'),
+#     path('upload-salary/', upload_salary, name='upload_salary'),
+#     path('save-rules/', save_rules)  
+# ]
 
 
 
